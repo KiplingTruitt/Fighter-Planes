aa2e45e16152e172d4dbae39cf13859aba0c0dd4
parkedcar2004 <parkerbohn212@gmail.com>
2025-03-29 16:51:43 -0400

    Enemy movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("CreateEnemyOne", 1, 2);
+        InvokeRepeating("CreateEnemyOne", 1, 3);
     }
 
     // Update is called once per frame
